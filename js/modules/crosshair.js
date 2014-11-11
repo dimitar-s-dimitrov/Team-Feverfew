2a393c521b6d133fc7ca6e0154ed9bffcdb8f26a
d0ntth1nc <d0ntth1nc@gmail.com>
2014-11-12 01:14:59 +0200

    performance and event handler fix

--- js/modules/crosshair.js
+++ js/modules/crosshair.js
@@ -1,13 +1,17 @@
 define("crosshair", function () {
     var $crosshair = $("#crosshair"),
         $gameField = $("#game-field"),
-        gameFieldOffset = $gameField.offset();
+        gameFieldOffset = $gameField.offset(),
+        crosshairWidth = $crosshair.width(),
+        crosshairHeight = $crosshair.height();
 
-    $gameField.on("mousemove", function(e) {
-        $crosshair.css({
-            top: e.pageY - gameFieldOffset.top - $crosshair.height(),
-            left: e.pageX - gameFieldOffset.left - $crosshair.width()
-        });
+    $gameField.on("mousemove", function (e) {
+        if (e.target == $gameField[0]) {
+            $crosshair.css({
+                top: e.pageY - gameFieldOffset.top - crosshairHeight,
+                left: e.pageX - gameFieldOffset.left - crosshairWidth
+            });
+        }
     });
 
     return {

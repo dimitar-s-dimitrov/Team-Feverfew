58cc494c2a9ac6733e32a48bda37ffda32d0fb3f
d0ntth1nc <d0ntth1nc@gmail.com>
2014-11-12 01:08:46 +0200

    init

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 // Setup the game
 $(function () {
     var game = require("game");
+    var crosshair = require("crosshair");
     
     game.startGame();
 });
--- js/modules/crosshair.js
+++ js/modules/crosshair.js
@@ -1,5 +1,16 @@
 define("crosshair", function () {
-    var $crosshair = $("#crosshair");
+    var $crosshair = $("#crosshair"),
+        $gameField = $("#game-field"),
+        gameFieldOffset = $gameField.offset();
 
+    $gameField.on("mousemove", function(e) {
+        $crosshair.css({
+            top: e.pageY - gameFieldOffset.top - $crosshair.height(),
+            left: e.pageX - gameFieldOffset.left - $crosshair.width()
+        });
+    });
 
+    return {
+        move: function () { }
+    };
 });

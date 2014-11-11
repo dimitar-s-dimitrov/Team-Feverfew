6e840a7f3dc4f98fa01cf1ced32a88cf49f789b0
d0ntth1nc <d0ntth1nc@gmail.com>
2014-11-12 00:44:45 +0200

    init

---
+++ js/modules/flyingNakov.js
@@ -0,0 +1,36 @@
+define("flyingNakov", function () {
+    function FlyingNakov(element, width, height) {
+        // Constructor logic
+        if (!element instanceof jQuery) {
+            element = $(element);
+        }
+        this.element = element;
+        this.width = width;
+        this.height = height;
+    }
+
+    // Public API
+    FlyingNakov.prototype = {
+
+        // Starts jquery moving animation
+        startMoving: function (speed) {
+
+        },
+
+        // Checks if the given coordinates are inside the object
+        isHit: function (shootXPos, shootYPos) {
+            var currentPosition = this.element.position();
+            if (shootXPos >= currentPosition.left &&
+                shootXPos <= currentPosition.left + this.width) {
+                if (shootYPos >= currentPosition.top &&
+                    shootYPos <= currentPosition.top + this.height) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    };
+
+    return FlyingNakov;
+});
